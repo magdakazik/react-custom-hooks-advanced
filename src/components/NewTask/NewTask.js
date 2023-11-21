cbc3871f5c610ec82170d1ec4aea53acfe785d15
Magda Kaźmierczak <magdakazik@gmail.com>
2023-11-21 09:52:17 +0100

    Add files via upload

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
+
+const NewTask = (props) => {
+  const {isLoading, error, sendRequest: sendTaskRequest} = useHttp()
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+  }
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest({
+      url: 'https://react-http-mk-default-rtdb.europe-west1.firebasedatabase.app/tasks.json', 
+      method: 'POST', 
+      headers: {'Content-Type': 'application/json'},
+      body: {text: taskText}
+    }, createTask.bind(null, taskText)) ///pre-configuration of function - taskText will be appended as a second argument on createTask
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
